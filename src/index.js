21c6077366e7c778abfd0a175b9b99baefc1c979
AnaRativa09 <anarativa09@gmail.com>
2020-06-10 08:56:58 -0500

    Eliminación comentarios datalist mes y año,implementación select options a través de JS

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const instrumentsPage = document.getElementById('instruments'); //pantalla 1
 const paymentPage = document.getElementById('payment'); //pantalla 2
 const buttonBuy = document.getElementById('btnBuy'); //Botón comprar
 const buttonDisabled = document.querySelector('.btnDisabled'); //Botón agotado, solo selecciona el primer elemento de la clase
+const form = document.getElementById('formCard');
 
+//Cambio de pantallas
 buttonBuy.addEventListener('click', showPayment);
 
 function showPayment() {
@@ -13,17 +15,28 @@ function showPayment() {
   instrumentsPage.classList.add('hidden');
 }
 
+// Alerta de no stock para botón "Agotado"
 buttonDisabled.addEventListener('click', noStock);
 
 function noStock() {
   alert('Muy pronto lo tendremos en stock');
 }
 
-
-/*const form = document.querySelector('#formCard');
+// Para crear menú desplegable en el select del mes
 for (let i = 1; i <= 12; i++) {
-    let opcion =document.creatElement("option");
-    opcion.value = i;
-    opcion.innerText = i;
-    form.selectMonth.appendChild('opcion');
-}*/
+    let optionMonths = document.createElement('option');
+    console.log(optionMonths);
+    optionMonths.value = i;
+    optionMonths.innerText = i;
+    form.selectMonth.appendChild(optionMonths);
+}
+
+// Para crear menú desplegable en el select del año
+const yearActual = new Date().getFullYear();
+for(let i = yearActual; i <= yearActual + 5; i++){
+	let optionYears = document.createElement('option');
+  console.log(yearActual);
+	optionYears.value = i;
+	optionYears.innerText = i;
+	form.selectYear.appendChild(optionYears);
+}
