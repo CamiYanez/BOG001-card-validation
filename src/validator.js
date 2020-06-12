118c1cb557bc9e1cd742c74a96be70201d71b676
AnaRativa09 <anarativa09@gmail.com>
2020-06-12 09:39:33 -0500

    Código validator sin modificaciones

--- src/validator.js
+++ src/validator.js
@@ -5,7 +5,7 @@ const validator = {
   isvalid: function(cardNumber){
     //Convertir el numero de la tdc en array y obtener el inverso
     let algorithmArray = cardNumber.split('').reverse();
-
+    
 //----------------------------- forEach ----------------------------------------------
     algorithmArray.forEach((element, index) => {
 
@@ -13,14 +13,14 @@ const validator = {
      if (index & 1) {
       element = element * 2
 
-      //A numeros mayores o iguales a 10, convertirlos en string y separar los dos digitos con split
+      //A numeros mayores o iguales a 10, convertirlos en string y separar los dos digitos con split 
       if (element >= 10) {
         element = element.toString().split('');
 
         //Convertir los digitos en numeros y sumarlos entre si
         element = parseInt(element[0]) + parseInt(element[1]);
       }
-    }
+    } 
 
     //Colocar lo numeros que quedan en cada posicion del array
     algorithmArray[index] = parseInt(element);
@@ -29,27 +29,23 @@ const validator = {
     let sum = algorithmArray.reduce(function(x,y){
       return x+y;
     });
-
+    
     //Declarar el residuo de la division entre 10
     let residue = sum % 10;
-
+    
     //Si el residuo es igual a 0 la tarjeta es valida, sino, es invalida
-      if (residue = 0) {
-        return true;
-      } else {
-        return false;
-      }
-  });
+    if (residue = 0) {
+      return true;
+    } else {
+      return false;
+    }     
+    });
   }
-
-    //maskify: (cardNumber) => {}
 };
 
 //----------------------- Codigo para pruebas ----------------------------------------
 // inputNumberCard.addEventListener('blur', function(){
 //   validator.isvalid(inputNumberCard.value)
 // });
-
-
 
 export default validator;
